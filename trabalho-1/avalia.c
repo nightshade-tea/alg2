93a41bc3603287aeccaf40f2e7e979fe51b4edcf
phfr24 <phfr24@inf.ufpr.br>
2024-11-02 21:18:40 -0300

    add calcula()

--- trabalho-1/avalia.c
+++ trabalho-1/avalia.c
@@ -4,28 +4,48 @@
 #include <math.h>
 #include "avalia.h"
 
-//------------------------------------------------------------------------------
-char *proximo_item(void) {
+#define ADD '+'
+#define SUB '-'
+#define MUL '*'
+#define DIV '/'
 
-  static char buffer[TAMANHO_MAXIMO_ITEM+1];
-
-  if ( scanf("%s", buffer) == EOF )
-
-    return NULL;
-  
-  return strcpy(malloc(sizeof(char)*(strlen(buffer)+1)), buffer);
-}
-//------------------------------------------------------------------------------
 // Sugestão: informe-se sobre as funções em ctype.h como
 //           isdigit(), isalpha() etc, bem como sobre as funções em
 //           stdlib.h como strtod() etc.
 
-double avalia(void) {
+static double calcula(char opr, double op1, double op2)
+{
+    switch (opr) {
+    case ADD:
+        return op1 + op2;
+    case SUB:
+        return op1 - op2;
+    case MUL:
+        return op1 * op2;
+    case DIV:
+        return op1 / op2;
+    }
 
-  char *item = proximo_item();
-
-  free(item);
-
-  return item == NULL ? NAN : 0;
+    return 0;
 }
 
+char *proximo_item(void)
+{
+    static char buffer[TAMANHO_MAXIMO_ITEM + 1];
+
+    if (scanf("%s", buffer) == EOF)
+
+        return NULL;
+
+    return strcpy(malloc(sizeof(char) * (strlen(buffer) + 1)), buffer);
+}
+
+double avalia(void)
+{
+    char *opr = proximo_item();
+
+    if (opr == NULL)
+        return NAN;
+
+    return 0;
+}
