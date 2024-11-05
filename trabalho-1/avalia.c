9479ea5b4dad3192b333b1e8f3905b671abcb7da
phfr24 <phfr24@inf.ufpr.br>
2024-11-05 11:01:05 -0300

    add log operation

--- trabalho-1/avalia.c
+++ trabalho-1/avalia.c
@@ -11,36 +11,44 @@
 #define DIV '/'
 #define EXP '^'
 #define MOD '%'
+#define LOG "log"
 
 /*
- * Verifica se o item é um operador (+, -, *, /, ^, %).
+ * Verifica se o item é um operador.
  *
- * Um item é um operador quando é uma string com apenas um caractere, o qual
- * deve representar alguma das operações aritméticas definidas.
+ * Um item é um operador quando é uma string que representa alguma das
+ * operações aritméticas definidas (+, -, *, /, ^, %, log).
  *
  * Retorno: 1 se é um operador ou 0 se não é um operador. Em caso de erro
  * retorna 0.
  */
 static int eh_operador(const char *item)
 {
-    if (item == NULL || strlen(item) != 1)
+    if (item == NULL)
         return 0;
 
-    switch (item[0]) {
-    case ADD:
-    case SUB:
-    case MUL:
-    case DIV:
-    case EXP:
-    case MOD:
-        return 1;
+    if (strlen(item) == 1) {
+        switch (item[0]) {
+        case ADD:
+        case SUB:
+        case MUL:
+        case DIV:
+        case EXP:
+        case MOD:
+            return 1;
+        }
+
+        return 0;
     }
+
+    if (strcmp(item, LOG) == 0)
+        return 1;
 
     return 0;
 }
 
 /*
- * Verifica se o item é um operando (número).
+ * Verifica se o item é um operando.
  *
  * Um item é um operando quando é uma string que representa um número válido,
  * podendo conter casas decimais.
@@ -66,29 +74,38 @@ static int eh_operando(const char *item)
  * Efetua o cálculo da operação aritmética opr entre os operandos op1 e op2,
  * respectivamente.
  *
+ * Para a operação log, op1 é o logaritmando e op2 é a base.
+ *
  * Retorno: o resultado da operação ou NAN em caso de erro.
  *
  * Exemplos:
- * calcula('+', 1, 1) retorna 2.
- * calcula('*', 2, 3) retorna 6.
- * calcula('/', 0, 0) retorna NAN.
+ * calcula("-", 5, 3) retorna 2.
+ * calcula("log", 8, 2) retorna 3.
+ * calcula("/", 0, 0) retorna NAN.
  */
-static double calcula(char opr, double op1, double op2)
+static double calcula(const char *opr, double op1, double op2)
 {
-    switch (opr) {
-    case ADD:
-        return op1 + op2;
-    case SUB:
-        return op1 - op2;
-    case MUL:
-        return op1 * op2;
-    case DIV:
-        return op1 / op2;
-    case EXP:
-        return pow(op1, op2);
-    case MOD:
-        return fmod(op1, op2);
+    if (strlen(opr) == 1) {
+        switch (opr[0]) {
+        case ADD:
+            return op1 + op2;
+        case SUB:
+            return op1 - op2;
+        case MUL:
+            return op1 * op2;
+        case DIV:
+            return op1 / op2;
+        case EXP:
+            return pow(op1, op2);
+        case MOD:
+            return fmod(op1, op2);
+        }
+
+        return NAN;
     }
+
+    if (strcmp(opr, LOG) == 0)
+        return log(op1) / log(op2);
 
     return NAN;
 }
@@ -108,23 +125,21 @@ double avalia(void)
     char *item = proximo_item();
 
     if (eh_operador(item)) {
-        char opr = item[0];
-        free(item);
-
         double op1 = avalia();
         double op2 = avalia();
+        double val = calcula(item, op1, op2);
 
-        return calcula(opr, op1, op2);
+        free(item);
+        return val;
     }
 
     if (eh_operando(item)) {
         double val = strtod(item, NULL);
-        free(item);
 
+        free(item);
         return val;
     }
 
     free(item);
-
     return NAN;
 }
